66cfb476a2003e49e4bb9e4dadf7df77ad27525b
Daniel Wright <daniel.wright1207@gmail.com>
2021-03-23 18:07:34 -0600

    finally done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,112 +1,69 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
-const jwt = require("jsonwebtoken");
-const bcryptjs = require("bcryptjs");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require("jsonwebtoken");
 const Users = require("../users/users-model");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", validateRoleName, (req, res, next) => {
   const credentials = req.body;
+
   if (credentials) {
     const rounds = process.env.BCRYPT_ROUNDS || 8;
-    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    const hash = bcrypt.hashSync(credentials.password, rounds);
+
     credentials.password = hash;
-    credentials.role_name = req.role_name;
+
     Users.add(credentials)
       .then((user) => {
         res.status(201).json(user);
       })
-      .catch((error) => {
-        res.status(500).json({ message: error.message });
+      .catch((err) => {
+        res.status(500).json({ message: err.message, stack: err.stack });
       });
   } else {
     res.status(400).json({
       message:
-        "please provide username and password and the password shoud be alphanumeric",
+        "please provide username and password, password must be alphanumeric",
     });
   }
-  // router.post("/register", validateRoleName, (req, res, next) => {
-  //   const { username, password, role_name } = req.body;
-
-  //   const hash = bcryptjs.hashSync(password, 10);
-  //   const userForDb = { username, password: hash, role_name };
-
-  //   Users.add(userForDb)
-  //     .then((user) => {
-  //       res.status(201).json(user);
-  //     })
-  //     .catch(next);
-  // });
-
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
 });
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  const { username, password } = req.body;
-
   if (req.body) {
+    const { username, password } = req.body;
+
     Users.findBy({ username: username })
       .then(([user]) => {
-        // compare the password the hash stored in the database
-        if (user && bcryptjs.compareSync(password, user.password)) {
-          // issue token
+        if (user && bcrypt.compareSync(password, user.password)) {
           const token = buildToken(user);
-          res.status(200).json({ message: `${username} is back`, token });
+          res.status(200).json({ message: `${user.username} is back!`, token });
         } else {
-          res.status(401).json({ message: "Invalid credentials" });
+          res.status(401).json({ message: "Invalid Credentials" });
         }
       })
-      .catch((error) => {
-        res.status(500).json({ message: error.message });
+      .catch((err) => {
+        res.status(500).json({ message: err.message, stack: err.stack });
       });
   } else {
     res.status(400).json({
       message:
-        "please provide username and password and the password shoud be alphanumeric",
+        "please provide username and password, password must be alphanumeric",
     });
   }
-
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
 });
+
 function buildToken(user) {
   const payload = {
-    // claims
     subject: user.user_id,
     username: user.username,
     role_name: user.role_name,
   };
-  const config = {
+  const configuration = {
     expiresIn: "1d",
   };
-  return jwt.sign(payload, JWT_SECRET, config);
+  return jwt.sign(payload, JWT_SECRET, configuration);
 }
 
 module.exports = router;
